90b6423d3b233d8b5653c231893d04e013574397
Atique Ahmed ANMEPL BLR <atique.ahmed@asianetnews.in>
2020-04-29 13:10:55 +0530

    github contribution graph hacks

---
+++ index.js
@@ -0,0 +1,31 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+const FILE_PATH = './data.json'
+const random = require('random');
+
+const makeCommit = n => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  // const DATE = moment().format(); //current date
+  const DATE = moment().subtract(1, 'y').add(1, 'd')
+               .add(x, 'w').add(y, 'd').format(); //Previous date
+
+  const data = {
+     date : DATE
+  }
+  console.log(DATE);
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    // git commit --date="" <----- to make previous date commit 
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE },
+    makeCommit.bind(this, --n));
+  });
+};
+
+makeCommit(100);
+
+
+
+
+
